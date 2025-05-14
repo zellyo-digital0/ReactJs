2fa0fee473fbbc5699d5298bdec9c4de7069932c
Zellyo Digital <zellyo.digital@gmail.com>
2025-05-14 13:24:03 +0600

    Complete 1st project

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -26,7 +26,6 @@ export default function Examples() {
   return (
     <Section title="Examples" id="examples">
       <Tabs
-        ButtonsContainer="menu"
         buttons={
           <>
             <TabButton
